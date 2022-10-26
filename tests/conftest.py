eec052814c692c8e4579279329b529526ea03722
hy.lee <hoyeol@mom-mom.net>
2022-10-26 19:30:49 +0900

    :wrench: config pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,49 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from app import main
+from app.config import get_settings
+from app.db import get_db, Base
+
+settings = get_settings()
+engine = create_engine(
+    "mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8mb4".format(
+        user=settings.db_user.get_secret_value(),
+        password=settings.db_password.get_secret_value(),
+        host="localhost",
+        port=settings.db_port,
+        database="test",
+    ),
+    pool_pre_ping=True,
+    echo=False,
+)
+TestingSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+
+@pytest.fixture(scope="session")
+def app():
+    def override_get_db():
+        db = TestingSessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    main.app.dependency_overrides[get_db] = override_get_db
+
+    return main.app
 
 
 @pytest.fixture
-def client():
-    yield TestClient(main.app)
+def client(app):
+    Base.metadata.create_all(bind=engine)
 
+    yield TestClient(app)
+
+    Base.metadata.drop_all(bind=engine)
